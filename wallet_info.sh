5886ed05520eacca001bea3f95c190a58511e215
SecorD0 <vovan6573@gmail.com>
2021-09-02 15:11:30 +0300

    Shows wallet info

--- wallet_info.sh
+++ wallet_info.sh
@@ -3,4 +3,4 @@ wallet_address=$(docker logs streamr_node | grep -oPm1 "(?<=Network node 'miner-
 wallet_info=$(wget -qO- "https://testnet1.streamr.network:3013/stats/$wallet_address")
 balance=$(jq ".claimCount" <<< $wallet_info)
 balance_percentage=$(jq ".claimPercentage" <<< $wallet_info)
-printf '{"wallet_address":"%s","balance":%d,"balance_percentage":%.2f}\n' "$wallet_address" "$balance" "$balance_percentage"
+printf '{"wallet_address":"%s","balance":%d,"balance_percentage":%f}\n' "$wallet_address" "$balance" "$balance_percentage"
